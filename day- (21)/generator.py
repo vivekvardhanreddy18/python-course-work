706ecc15efb345e0827ea2ee7dda90a6ad33e07b
Vivek vardhan reddy doma <domavivekreddy@gmail.com>
2026-09-02 11:18:21 +0000

    Sep 2, 2026, 4:48 PM

--- day- (21)/generator.py
+++ day- (21)/generator.py
@@ -1,11 +1,23 @@
-def reels():
-    data = ['reel1', 'reel2', 'reel3', 'reel4', 'reel5']
-    for i in data:
-        yield i
+# def reels():
+#     data = ['reel1', 'reel2', 'reel3', 'reel4', 'reel5']
+#     for i in data:
+#         yield i
 
-res = reels()
-print(next(res))
-print(next(res))
-print(next(res))
-print(next(res))
-print(next(res))
+# res = reels()
+# print(next(res))
+# print(next(res))
+# print(next(res))
+# print(next(res))
+# print(next(res))
+
+def countdown():
+    yield 5
+    yield 4
+    yield 3
+    yield 2
+    yield 1
+
+res = countdown()
+for i in res:
+    print(i)
+    
