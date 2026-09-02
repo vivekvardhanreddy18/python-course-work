31e83c16580407fbd250f34479310463b1079800
Vivek vardhan reddy doma <domavivekreddy@gmail.com>
2026-09-02 11:59:53 +0000

    Sep 2, 2026, 5:29 PM

--- day- (21)/listcomprehension.py
+++ day- (21)/listcomprehension.py
@@ -1,11 +1,11 @@
-l = [i for i in range(1,11)]
-print(l)
+# l = [i for i in range(1,11)]
+# print(l)
 
-t= [i for i in range(2,11,2)]
-print(t)
+# t= [i for i in range(2,11,2)]
+# print(t)
 
-n=16
-s=[i for i in range(1,n+1) if n%i==0]
-print(s)
+# n=16
+# s=[i for i in range(1,n+1) if n%i==0]
+# print(s)
 
 
