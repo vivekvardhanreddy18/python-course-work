a831ff105eb87d972bde3561239cb40c45fe1fc3
Vivek vardhan reddy doma <domavivekreddy@gmail.com>
2026-09-03 12:07:52 +0000

    Sep 3, 2026, 5:37 PM

--- day- (22)/claculator/ATM/logic.py
+++ day- (22)/claculator/ATM/logic.py
@@ -1,3 +1,47 @@
 data = {
-    123456:{''}
+    123456:{'name':"Vivek",'pin':1234,'balance':10000,'history':[]},
+    789012:{'name':"vinith",'pin':5678,'balance':15000,'history':[]},
+    342354:{'name':"rushikesh",'pin':9012,'balance':20000,'history':[]},
+    163777:{'name':"karthik",'pin':3456,'balance':25000,'history':[]},
 }
+
+def login():
+    global acc_num
+    acc_num = int(input("Enter your account number: "))
+    pin = int(input("Enter your PIN: "))
+    if acc_num in data and data[acc_num]['pin'] == pin:
+        print("Login successful!\n\nWelcome, ", data[acc_num]['name'])
+        return True
+    else:
+        print("Invalid credentials.")
+        
+def menu():
+    print("\nMenu:")
+    print("[C]heck Balance")
+    print("[D]eposit")
+    print("[W]ithdraw")
+    print("[V]iew Transaction History")
+    print("[E]xit")
+
+def check_balance():
+    print("Your current balance is: ", data[acc_num]['balance'])
+
+def deposit():
+    amount = float(input("Enter the amount to deposit: "))
+    data[acc_num]['balance'] += amount
+    data[acc_num]['history'].append(('Deposit', amount))
+    print("Amount deposited successfully!\nYour new balance is: ", data[acc_num]['balance'])
+
+def withdraw():
+    amount = float(input("Enter the amount to withdraw: "))
+    if data[acc_num]['balance'] >= amount:
+        data[acc_num]['balance'] -= amount
+        data[acc_num]['history'].append(('Withdrawal', amount))
+        print("Amount withdrawn successfully!\nYour new balance is: ", data[acc_num]['balance'])
+    else:
+        print("Insufficient balance.")
+
+def view_history():
+    print("Transaction History:")
+    for transaction in data[acc_num]['history']:
+        print(f"{transaction[0]}: ${transaction[1]}")
