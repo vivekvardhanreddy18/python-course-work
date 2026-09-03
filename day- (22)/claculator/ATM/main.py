21f2c898a3f5de251a51beddf6acce04ac1ae183
Vivek vardhan reddy doma <domavivekreddy@gmail.com>
2026-09-03 12:30:18 +0000

    Sep 3, 2026, 6:00 PM

--- day- (22)/claculator/ATM/main.py
+++ day- (22)/claculator/ATM/main.py
@@ -1,19 +1,19 @@
 import logic as lg
 
-if lg.login():
-    while lg.login():
-        print(lg.menu())
-        choice = input("Enter your choice: ").lower()
-        if choice == 'c':
-            lg.check_balance()
-        elif choice == 'd':
-            lg.deposit()
-        elif choice == 'w':
-            lg.withdraw()
-        elif choice == 'v':
-            lg.view_history()
-        elif choice == 'e':
-            print("Thank you for using our ATM. visit again!")
+
+while lg.login():
+    print(lg.menu())
+    choice = input("Enter your choice: ").lower()
+    if choice == 'c':
+        lg.check_balance()
+    elif choice == 'd':
+        lg.deposit()
+    elif choice == 'w':
+        lg.withdraw()
+    elif choice == 'v':
+        lg.view_history()
+    elif choice == 'e':
+        print("Thank you for using our ATM. visit again!")
         break
     else:
         print("Invalid choice. Please try again.")
