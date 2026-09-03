d5f0022fd8d1e5df80e4c908eb314a4c9e3dfb1a
Vivek vardhan reddy doma <domavivekreddy@gmail.com>
2026-09-03 11:16:49 +0000

    Sep 3, 2026, 4:46 PM

--- day- (22)/claculator/main.py
+++ day- (22)/claculator/main.py
@@ -1,8 +1,20 @@
-import logic
+# import logic
 
-logic.add(5, 4)
-logic.subtract(5, 4)
-logic.multiply(5, 4)
-logic.divide(5, 4)
-logic.power(5, 4)
-logic.modulus(5, 4)
+# logic.add(5, 4)
+# logic.subtract(5, 4)
+# logic.multiply(5, 4)
+# logic.divide(5, 4)
+# logic.power(5, 4)
+# logic.modulus(5, 4)
+
+
+#  if file name is larger then we can use alias name like follows
+
+import logic as l
+
+l.add(5, 4)
+l.subtract(5, 4)
+l.multiply(5, 4)
+l.divide(5, 4)
+l.power(5, 4)
+l.modulus(5, 4)
