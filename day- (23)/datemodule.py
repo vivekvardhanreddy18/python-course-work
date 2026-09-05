1d3440bdee048f756b011a66a982685d8fe0f580
Vivek vardhan reddy doma <domavivekreddy@gmail.com>
2026-09-05 12:04:25 +0000

    Sep 5, 2026, 5:34 PM

--- day- (23)/datemodule.py
+++ day- (23)/datemodule.py
@@ -13,4 +13,9 @@ from datetime import date,time,datetime,timedelta
 # print(date(year,month,day))
 
 
-tm = time()
+tm = time(12,30,45)
+
+print(tm)
+print(tm.hour)
+print(tm.minute)
+print(tm.second)
