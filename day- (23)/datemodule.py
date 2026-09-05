2f863a83f0c2090a6a4e51335d99c7dda0f7fc14
Vivek vardhan reddy doma <domavivekreddy@gmail.com>
2026-09-05 11:54:32 +0000

    Sep 5, 2026, 5:24 PM

---
+++ day- (23)/datemodule.py
@@ -0,0 +1,13 @@
+from datetime import date,time,datetime,timedelta
+
+# t=date.today()
+
+# print(t)
+# print(t.day)
+# print(t.month)
+# print(t.year)
+# print(t.weekday())
+
+year,month,day = list(map(int,input("Enter date in YYYY-MM-DD format: ").split('-')))
+
+print(date(year,month,day))
