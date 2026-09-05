f384be6390b1fb36f9176c209bc546c8a4e0f304
Vivek vardhan reddy doma <domavivekreddy@gmail.com>
2026-09-05 11:27:53 +0000

    Sep 5, 2026, 4:57 PM

--- day- (23)/builtinmodules.py
+++ day- (23)/builtinmodules.py
@@ -1,18 +1,6 @@
 import random
 import string
 
-name = input("Enter your name: ").strip().replace(" ", "")
-dob = input("Enter your date of birth: ").strip()
-
-password = list(name + dob)
-password += [
-	random.choice(string.ascii_uppercase),
-	random.choice(string.ascii_lowercase),
-	random.choice(string.digits),
-	random.choice("!@#$%^&*"),
-]
-password += random.choices(
-	string.ascii_letters + string.digits + "!@#$%^&*", k=4
-)
-random.shuffle(password)
-print("Generated password:", "".join(password))
+characters = string.ascii_letters + string.digits + "!@#$%^&*"
+password = "".join(random.choices(characters, k=12))
+print("Generated password:", password)
