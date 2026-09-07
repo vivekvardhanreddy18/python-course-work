06079c7c913d87aa06ad33bba1c8e8f3b1dbfb73
Vivek vardhan reddy doma <domavivekreddy@gmail.com>
2026-09-07 12:00:22 +0000

    Sep 7, 2026, 5:30 PM

--- day- (24)/oops.py
+++ day- (24)/oops.py
@@ -1,7 +1,14 @@
 class flipkart:
     discount = 0.1  # 10% discount
 
+    def info(self,name, phno,address):
+        self.name = name
+        self.phno = phno
+        self.address = address
+        print("Welcome to Flipkart!",self.name)
+
+
 vivek = flipkart()
 vinith = flipkart()
-vivek.name = "vivek"
-vinith.name = "vinith"
+vivek.info("vivek", 1234567890, "123 Main St")
+vinith.info("vinith", 9987654321, "456 Oak Ave")
