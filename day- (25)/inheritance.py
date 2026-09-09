7c7a48975f8d1025a11927bdafa1ff2ab875bd85
Vivek vardhan reddy doma <domavivekreddy@gmail.com>
2026-09-09 11:55:03 +0000

    Sep 9, 2026, 5:25 PM

--- day- (25)/inheritance.py
+++ day- (25)/inheritance.py
@@ -2,5 +2,14 @@ class whatsappv1:
     def messege(x):
         print('You can send messege to your friends')
 
+
+class whatsappv2(whatsappv1):
+    def status(x):
+        print('You can upload status to your friends')
 vinith = whatsappv1()
 vinith.messege()
+
+vivek = whatsappv2()
+vivek.messege() 
+vivek.status()
+
