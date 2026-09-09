59ed67e30a30e0986e639bcf3847487fbd26606a
Vivek vardhan reddy doma <domavivekreddy@gmail.com>
2026-09-09 11:21:54 +0000

    Sep 9, 2026, 4:51 PM

---
+++ day- (25)/encapsulation.py
@@ -0,0 +1,20 @@
+class Instagram:
+    def __init__(x,username,pw):
+        x.username = username
+        x.__pw = pw
+        x._post = []
+
+    def getpw():
+        return x.__pw
+
+    @property
+    def accesspost(x):
+        return x._post
+
+    
+
+
+
+
+
+
