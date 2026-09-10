b910c2925d32b4aa42624b752643967852d4a9c5
Vivek vardhan reddy doma <domavivekreddy@gmail.com>
2026-09-10 11:41:24 +0000

    Sep 10, 2026, 5:11 PM

---
+++ day- (26)/polymorphism.py
@@ -0,0 +1,40 @@
+class hotstar:
+    def __init__(x,name):
+        print(f'Welcome to Hotstar, {name}')
+
+    def auth(x):
+        print("yes you can login")
+
+    def dashboard(x):
+        print("You can see it")
+
+    def search(x):
+        print("You can search")
+
+    def history(x):
+        print("You can see history")
+
+    def playcontrol(x):
+        print("play, pause, resume")
+
+    def ads(x):
+        print("ads will run")
+
+    def quality(x):
+        print("You have limitrd quality")
+
+    def devices(x):
+        print("limited login")
+
+
+
+class premiumhotstar(hotstar):
+    def ads(x):
+        print("ads wont run")
+
+    def quality(x):
+        print("high quality")
+
+    def devices(x):
+        print("Multiple logins")
+        
