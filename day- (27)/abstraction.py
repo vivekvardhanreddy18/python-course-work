03cc5c6f3a7f377b0ac71786df9fadf740bb3f76
Vivek vardhan reddy doma <domavivekreddy@gmail.com>
2026-09-11 11:29:48 +0000

    Sep 11, 2026, 4:59 PM

---
+++ day- (27)/abstraction.py
@@ -0,0 +1,45 @@
+from abc import ABC,abstractmethod
+
+
+class payment(ABC):
+    def source(x):
+        print("Scanner")
+    def amount(x):
+        print("Enter the amount")
+    def bank(x):
+        print("Select the bank")
+    def pin(x):
+        print("Enter the pin")
+    @abstractmethod
+    def paymentprocess(x):
+        pass
+    def paymentstatus(x):
+        print("Payment successfull")
+
+class HDFC(Payment):
+    def paymentprocess(x):
+        print("Payment is processed through HDFC")
+class ICICI(Payment):
+    def paymentprocess(x):
+        print("Payment is processed through ICICI")
+class UNION(Payment): 
+    def paymentprocess(x):
+        print("Payment is processed through UNION")
+class AXIS(Payment):   
+    def paymentprocess(x):
+        print("Payment is processed through AXIS")
+
+
+vivek=HDFC()
+
+
+    
+
+    
+
+
+
+
+
+
+
