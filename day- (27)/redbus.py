b87f46e96d0136eef59b18d87910475863bb672d
Vivek vardhan reddy doma <domavivekreddy@gmail.com>
2026-09-11 12:01:16 +0000

    Sep 11, 2026, 5:31 PM

--- day- (27)/redbus.py
+++ day- (27)/redbus.py
@@ -1,2 +1,18 @@
 class redbus:
-    bus={}
+    bus={i: "Available" for i in range(1,11)}
+
+    def displayseat(x):
+        print("--------MVR Travels--------")
+        for i in redbus.bus:
+            print(i,redbus.bus[i])
+
+    def booking(x,seatno):
+        for i in redbus.bus:
+            if i==seatno and redbus.bus[i]=='Available':
+                redbus.bus[i] = 'Booked'
+                print(f"Your {seatno} is successfully booked")
+        else:
+            print("Seat is already booked")
+            
+
+        
