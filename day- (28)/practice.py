906a54c4d7649bd46d6d1469f644dc1675ccca61
Vivek vardhan reddy doma <domavivekreddy@gmail.com>
2026-09-19 06:15:12 +0000

    Sep 19, 2026, 11:45 AM

--- day- (28)/practice.py
+++ day- (28)/practice.py
@@ -4,7 +4,12 @@
 # for i in range(6):
 #     s+=i
 # print(s)
-s = 0
-for i in range(0,11,2):
-    s+=i
-print(s)
+# s = 0
+# for i in range(0,11,2):
+#     s+=i
+# print(s)
+a = (1,2,3,4)
+b = (1,2,3)
+a +=b
+print(a)
+print(type(a))
