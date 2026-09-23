aa48d461db76a523f0afb6416d26067e0688c539
Vivek vardhan reddy doma <domavivekreddy@gmail.com>
2026-09-23 11:05:27 +0000

    Sep 23, 2026, 4:35 PM

--- day- (29)/regex.py
+++ day- (29)/regex.py
@@ -6,5 +6,5 @@ text = 'codegnan2026'
 res = re.match(pattern, text)
 res = re.search(pattern, text)
 res = re.findall(pattern, text)
-
-print(res.group() if res else "Pattern not matched")
+print(res)
+# print(res.group() if res else "Pattern not matched")
